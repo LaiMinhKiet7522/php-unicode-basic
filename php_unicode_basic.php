fd08781514fd54541ab309f83d9295e7ddc74b3e
Minh Kiet <kietminh070502@gmail.com>
2023-09-17 20:45:08 +0700

    Count the number of occurrences of characters in a string

--- php_unicode_basic.php
+++ php_unicode_basic.php
@@ -4,99 +4,54 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Customer</title>
-
+    <title>Charactor Count</title>
 </head>
 
 <body>
     <?php
-    $customerArr = [
-        [
-            'name' => 'Minh Kiệt',
-            'email' => 'laiminhkiet07052002@gmail.com',
-            'phone' => '0376707091',
-            'address' => 'Ho Chi Minh city, Viet Nam',
-        ],
-    
-        [
-            'name' => 'Hoàng Phúc',
-            'email' => 'hoangphuc27052002@gmail.com',
-            'phone' => '0998877321',
-            'address' => 'Ho Chi Minh city, Viet Nam',
-        ],
-
-        [
-            'name' => 'Hoàng Phúc',
-            'email' => 'hoangphuc27052002@gmail.com',
-            'phone' => '0998877321',
-            'address' => 'Ho Chi Minh city, Viet Nam',
-        ],
-    
-        [
-            'name' => 'Vĩnh Thông',
-            'email' => 'vinhthong07082002@gmail.com',
-            'phone' => '0353218146',
-            'address' => 'Ho Chi Minh city, Viet Nam',
-        ],
-
-        [
-            'name' => 'Vĩnh Thông',
-            'email' => 'vinhthong07082002@gmail.com',
-            'phone' => '0353218146',
-            'address' => 'Ho Chi Minh city, Viet Nam',
-        ]
-    ];
-    if (!empty($customerArr)) {
-        //Tìm phần tư mảng trùng
-        $indexDuplicate = [];
-        for ($i = 0; $i < count($customerArr) - 1; $i++) {
-            for ($j = $i + 1; $j < count($customerArr); $j++) {
-                if ($customerArr[$i]['email'] == $customerArr[$j]['email']) {
-                    $indexDuplicate[] = $j;
-                }
-            }
-        }
-    
-        //Xóa phần tử mảng trùng
-        if (!empty($indexDuplicate)) {
-            foreach ($indexDuplicate as $index) {
-                if (isset($customerArr[$index])) {
-                    unset($customerArr[$index]);
-                }
+    $content = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
+    $resultArr = [];
+    if (strlen($content) > 0) {
+        for ($i = 0; $i < strlen($content); $i++) {
+            $char = $content[$i];
+            if (!empty($resultArr[$char])) {
+                $resultArr[$char]++;
+            } else {
+                $resultArr[$char] = 1;
             }
         }
     }
+    
+    echo '<pre>';
+    print_r($resultArr);
+    echo '</pre>';
     ?>
-    <table width="100%" border="1" cellpadding="0" cellspacing="0">
+    <table width="100%" border="1" cellspacing="0" cellpadding="0">
         <thead>
             <tr>
                 <th width="5%">STT</th>
-                <th>Họ và tên</th>
-                <th>Email</th>
-                <th>Điện thoại</th>
-                <th>Địa chỉ</th>
+                <th>Ký tự</th>
+                <th width="15%">Số lần xuất hiện</th>
             </tr>
         </thead>
         <tbody>
             <?php 
-                if(!empty($customerArr) && is_array($customerArr)):
+                if(!empty($resultArr)):
                     $count = 0;
-                    foreach($customerArr as $item):
+                    foreach($resultArr as $key=>$value):
                         $count++;
             ?>
             <tr>
-                <td><?php echo $count; ?></td>
-                <td><?php echo $item['name']; ?></td>
-                <td><?php echo $item['email']; ?></td>
-                <td><?php echo $item['phone']; ?></td>
-                <td><?php echo $item['address']; ?></td>
+                <td width="5%"><?php echo $count; ?></td>
+                <td><?php echo $key == ' ' ? 'Khoảng trắng' : $key; ?></td>
+                <td><?php echo $value; ?></td>
             </tr>
             <?php 
-                endforeach;
-            else:
+            endforeach;
+        else:
             ?>
             <tr>
-                <td colspan="5" style="text-align: center;">Không có dữ liệu</td>
+                <td colspan="3" style="text-align: center;">Không có kết quả</td>
             </tr>
             <?php 
             endif;
