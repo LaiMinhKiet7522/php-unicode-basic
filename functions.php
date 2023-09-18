733a49283195788d2c348f3d1caba486235c2e15
Minh Kiet <kietminh070502@gmail.com>
2023-09-18 19:12:20 +0700

    Display multi-level menu using recursive algorithm part 2

--- functions.php
+++ functions.php
@@ -14,4 +14,28 @@ function buildMenu($menuArr, $isSub = false)
         echo '</ul>';
     }
 }
+
+function makeMenu($menuArr, $parentId = 0, $isSub = false)
+{
+    $childArr = [];
+    if (!empty($menuArr)) {
+        foreach ($menuArr as $key => $item) {
+            if ($item['parent'] == $parentId) {
+                $childArr[] = $item;
+                unset($menuArr[$key]);
+            }
+        }
+    }
+
+    if (!empty($childArr)) {
+        echo $isSub ? '<ul class="sub-menu">' : '<ul class="menu">';
+        foreach ($childArr as $key => $item) {
+            echo '<li>';
+            echo '<a href="' . $item['link'] . '">' . $item['title'] . '</a>';
+            makeMenu($menuArr, $item['id'], true);
+            echo '</li>';
+        }
+        echo '</ul>';
+    }
+}
 ?>
--- php_unicode_basic.php
+++ php_unicode_basic.php
@@ -2,70 +2,77 @@
 require_once 'functions.php';
 $menuArr = [
     [
+        'id' => 1,
         'title' => 'Trang chủ',
         'link' => '#',
+        'parent' => 0,
     ],
 
     [
+        'id' => 2,
         'title' => 'Giới thiệu',
         'link' => '#',
+        'parent' => 0,
     ],
 
     [
+        'id' => 3,
         'title' => 'Dịch vụ',
         'link' => '#',
-        'sub' => [
-            [
-                'title' => 'Thiết kế web',
-                'link' => '#',
-                'sub' => [
-                    [
-                        'title' => 'Web App',
-                        'link' => '#',
-                    ],
-
-                    [
-                        'title' => 'Website',
-                        'link' => '#',
-                    ],
-                ],
-            ],
-
-            [
-                'title' => 'Dịch vụ SEO',
-                'link' => '#',
-            ],
-
-            [
-                'title' => 'Phần mềm',
-                'link' => '#',
-                'sub' => [
-                    [
-                        'title' => 'Quản lý bán hàng',
-                        'link' => '#',
-                    ],
-
-                    [
-                        'title' => 'Quản lý khách hàng',
-                        'link' => '#',
-                    ],
-                ],
-            ],
-        ],
+        'parent' => 0,
     ],
 
     [
+        'id' => 4,
+        'title' => 'Thiết kế web',
+        'link' => '#',
+        'parent' => 3,
+    ],
+
+    [
+        'id' => 5,
+        'title' => 'Dịch vụ SEO',
+        'link' => '#',
+        'parent' => 3,
+    ],
+
+    [
+        'id' => 6,
+        'title' => 'Phần mềm',
+        'link' => '#',
+        'parent' => 3,
+    ],
+
+    [
+        'id' => 7,
+        'title' => 'Web bán hàng',
+        'link' => '#',
+        'parent' => 4,
+    ],
+
+    [
+        'id' => 8,
+        'title' => 'Web tin tức',
+        'link' => '#',
+        'parent' => 4,
+    ],
+
+    [
+        'id' => 9,
         'title' => 'Tin tức',
         'link' => '#',
+        'parent' => 0,
     ],
 
     [
+        'id' => 10,
         'title' => 'Liên hệ',
         'link' => '#',
+        'parent' => 0,
     ],
 ];
 
-buildMenu($menuArr);
+makeMenu($menuArr);
 ?>
 
 <!-- <ul class="menu">
