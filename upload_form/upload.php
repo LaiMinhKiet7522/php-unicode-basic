74730f4e582cf7965903faefb41209ef7a608c07
Minh Kiet <kietminh070502@gmail.com>
2023-10-06 19:32:23 +0700

    upload file part 2

--- upload_form/upload.php
+++ upload_form/upload.php
@@ -2,38 +2,57 @@
 //Kiểm tra phương thức POST
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+    $errors = []; // Mảng lưu trữ lỗi upload
+    $allowedArr = [
+        'mp4', 'png', 'gif', 'jpeg', 'jpg'
+    ];
+    $maxFileSize = 3145728;
+
     //Lấy thông tin file
     if (!empty($_FILES['file_upload'])) {
 
-        //Lấy tên file
-        $fileName = $_FILES['file_upload']['name'];
+        //Kiểm tra xem file đã được chọn chưa
+        if ($_FILES['file_upload']['error'] == 4) {
+            $errors['choose_file'] = 'Vui lòng chọn file';
+        } else {
 
-        //Đổi tên file
-        $fileNameArr = explode('.', trim($fileName));
-        $filExtension = end($fileNameArr);
-        $fileBefore = sha1(uniqid());
-        $fileName = $fileBefore . '.' . $filExtension;
+            //Lấy tên file
+            $fileName = $_FILES['file_upload']['name'];
 
-        //
-        $allowedArr = [
-            'mp4', 'png', 'gif', 'jpeg', 'jpg'
-        ];
-        if (in_array($filExtension, $allowedArr)) {
+            //Đổi tên file
+            $fileNameArr = explode('.', trim($fileName));
+            $filExtension = end($fileNameArr);
+            $fileBefore = sha1(uniqid());
+            $fileName = $fileBefore . '.' . $filExtension;
+
+            //Kiểm tra định dạng file
+            if (!in_array($filExtension, $allowedArr)) {
+                $errors['allow_ext'] = 'Định dạng không đúng, chỉ chấp nhận ' . implode(', ', $allowedArr);
+            }
 
             //Kiểm tra dung lượng file
-            $size = $_FILES['file_upload']['size'];
-            if ($size <= 3145728) {
-                $upload = move_uploaded_file($_FILES['file_upload']['tmp_name'], './uploads/' . $fileName);
-                if ($upload) {
-                    echo 'Upload thành công';
-                } else {
-                    echo 'Upload không thành công';
+            if (!empty($_FILES['file_upload']['size'])) {
+                $size = $_FILES['file_upload']['size'];
+                if ($size > $maxFileSize) {
+                    $errors['max_size'] = 'Dung lượng vượt quá cho phép, chỉ được upload <= ' . number_format($maxFileSize) .' bytes';
                 }
             } else {
-                echo 'Dung lượng vượt quá cho phép';
+                $errors['file_error'] = 'File bị lỗi, vui lòng kiểm tra lại';
+            }
+        }
+
+        //Kiểm tra mảng errors để tiến hành upload
+        if (empty($errors)) {
+            $upload = move_uploaded_file($_FILES['file_upload']['tmp_name'], './uploads/' . $fileName);
+            if ($upload) {
+                echo 'Upload thành công';
+            } else {
+                echo 'Upload thất bại';
             }
         } else {
-            echo 'Kiểu file không hợp lệ';
+            foreach ($errors as $item) {
+                echo $item . '<br/>';
+            }
         }
     }
 }
