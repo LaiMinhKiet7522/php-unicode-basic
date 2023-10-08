8d1582b2d2cd56e1d7af815651be37c68899658d
Minh Kiet <kietminh070502@gmail.com>
2023-10-08 14:08:33 +0700

    cookie in php part 2

--- cookie-session/cookie.php
+++ cookie-session/cookie.php
@@ -2,11 +2,42 @@
 //Thiết lập cookie
 setcookie('username', 'Minh Kiệt', time() + 86400, '/', '', false, true);
 
+$data = [
+    'item 1',
+    'item 2'
+];
+$dataStr = json_encode($data);
+
 //Update cookie
-setcookie('username', 'minh kiet', time() + 86400, '/', '', false, true);
+setcookie(
+    'username',
+    $dataStr,
+    time() + 86400,
+    '/',
+    '',
+    false,
+    true
+);
 
 //Xóa cookie
-setcookie('username', 'minh kiet', time() - 60, '/', '', false, true);
+// setcookie('username', 'minh kiet', time() - 60, '/', '', false, true);
 
 //Lấy cookie
-echo $_COOKIE["username"];
+$dataStr =  $_COOKIE['username'];
+echo $dataStr;
+echo '<br/>';
+$data = json_decode($dataStr, true);
+echo '<pre>';
+print_r($data);
+echo '</pre>';
+
+/*
+ * Chức năng sản phẩm đã xem
+ * - Khi xem sản phẩm 1 => Lấy id sản phẩm => Lưu id vào cookie
+ * - Khi xem sản phẩm 2 => Lấy id sản phẩm => Lấy id cũ của cookie => Thêm id mới => Lưu vào cookie
+ *
+ * */
+
+//Lưu ý:
+// -Không lưu các thông tin quan trọng ở cookie như: Mật khẩu, tài khoản ngân hàng...
+// -Cookie không chấp nhận các giá trị phức hợp như Mảng, Object, các kiểu null, '',
