ec88630a7056d20131e50fa8a0bd0915ecda785b
Minh Kiet <kietminh070502@gmail.com>
2023-10-12 19:03:42 +0700

    Fetch data using pdo

--- php_mysql_pdo/insert.php
+++ php_mysql_pdo/insert.php
@@ -1,15 +1,16 @@
 <?php
 require_once "connect.php";
-$sql = "INSERT INTO1 users(email,fullname,created_at) VALUES (:email, :fullname, :created_at)";
+$sql = "INSERT INTO users(email,fullname,created_at) VALUES (:email, :fullname, :created_at)";
 try {
     $statement = $conn->prepare($sql);
-    $email = 'laiminhkiet07052002@gmail.com';
-    $fullname = 'Lai Minh Kiet';
+    $email = 'nguyenvana@gmail.com';
+    $fullname = 'Nguyen Van A';
     $created_at = date('Y-m-d H:i:s');
     $statement->bindParam(':email', $email);
     $statement->bindParam(':fullname', $fullname);
     $statement->bindParam(':created_at', $created_at);
     $insert_status = $statement->execute();
+    echo 'Id vừa Insert: '.$conn->lastInsertId().'<br/>';
 } catch (Exception $exception) {
     echo 'Error: ' . $exception->getMessage() . '<br/>';
     echo 'File: ' . $exception->getFile() . '<br/>';
